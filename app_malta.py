450455b493d332515ee3573470e75ad332670b6b
Nicholas Serrano <161484895+NicholasSerrano@users.noreply.github.com>
2024-05-07 16:30:15 +0200

    Update aeliminar libreria matplotlibpp_malta.py
    eliminar libreria matplotlib

--- app_malta.py
+++ app_malta.py
@@ -4,8 +4,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
-
 
 # --------------------CONFIGURACIÓN DE LA PÁGINA----------------------------#
 st.set_page_config(
